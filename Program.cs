2970212fe75a61ee69813bba610d3726d130e697
Mjolner Asgardson <mjolnerasgardson@gmail.com>
2024-09-11 23:44:24 +1200

    Создала 2 массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] array1 = new string[6] { "Mother", "356", "You", "-8", "Lu", "21" };
+        string[] array2 = new string[array1.Length];
+
+        ComparisonArray(array1, array2);
+        PrintArray(array2);
+    }
