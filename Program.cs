73adbec78dd9d224a792e8e2554c10c464537955
Mjolner Asgardson <mjolnerasgardson@gmail.com>
2024-09-11 23:45:58 +1200

    Сделала решение и вывод результата

--- Program.cs
+++ Program.cs
@@ -8,3 +8,28 @@
         ComparisonArray(array1, array2);
         PrintArray(array2);
     }
+    static void ComparisonArray(string[] array1, string[] array2)
+    {
+        int count = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+            {
+                array2[count] = array1[i];
+                count++;
+            }
+        }
+    }
+
+    static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] != null)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
